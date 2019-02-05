ac31aa9967c93d0303ed290fa4890f099598138a
Nik <nik@sunraystrawberries.com.au>
2019-02-06 09:35:15 +1000

    MyGet baby!

---
+++ pushPackage.ps1
@@ -0,0 +1,26 @@
+# watch the bug in VS where PackageVersion in the csproj file is not PackageVersion in the properties UI
+# 1. update version in .csproj for netcore libs and AssemblyInfo.cs for framework libs
+# 2. change the version below
+$ver = "0.2.1"
+$nuserver = "https://www.myget.org/F/yummy-ag/api/v2/package"
+
+Write-Host -ForegroundColor Cyan "Punfai.Report..."
+nuget push .\Punfai.Report.$ver.nupkg 17fcb248-be21-431b-9e69-15e4b0396c5a -Source $nuserver
+Remove-Item .\Punfai.Report.$ver.nupkg
+
+Write-Host -ForegroundColor Cyan "IronScript"
+nuget push .\Punfai.Report.IronScript.$ver.nupkg 17fcb248-be21-431b-9e69-15e4b0396c5a -Source $nuserver
+Remove-Item .\Punfai.Report.IronScript.$ver.nupkg
+
+Write-Host -ForegroundColor Cyan "OfficeOpenXml..."
+nuget push .\Punfai.Report.OfficeOpenXml.$ver.nupkg 17fcb248-be21-431b-9e69-15e4b0396c5a -Source $nuserver
+Remove-Item .\Punfai.Report.OfficeOpenXml.$ver.nupkg
+
+Write-Host -ForegroundColor Cyan "Ibex..."
+nuget push .\Punfai.Report.Ibex.$ver.nupkg 17fcb248-be21-431b-9e69-15e4b0396c5a -Source $nuserver
+Remove-Item .\Punfai.Report.Ibex.$ver.nupkg
+
+Write-Host -ForegroundColor Cyan "Wpf..."
+nuget push .\Punfai.Report.Wpf.$ver.nupkg 17fcb248-be21-431b-9e69-15e4b0396c5a -Source $nuserver
+Remove-Item .\Punfai.Report.Wpf.$ver.nupkg
+Remove-Item .\Punfai.Report.Wpf.$ver.symbols.nupkg
--- repackage.ps1
+++ repackage.ps1
@@ -1,30 +1,22 @@
 # watch the bug in VS where PackageVersion in the csproj file is not PackageVersion in the properties UI
-$ver = "0.2.0"
-$nuserver = "Z:\0005 IT\Software\nuget"
+# 1. update version in .csproj for netcore libs and AssemblyInfo.cs for framework libs
+# 2. change the version below
+# 3. change the version in pushPackage.ps1
+
+$ver = "0.2.1"
+$nuserver = "https://www.myget.org/F/yummy-ag/api/v2/package"
+
 Write-Host -ForegroundColor Cyan "Punfai.Report..."
-dotnet pack .\src\Punfai.Report --configuration Release
-nuget delete Punfai.Report $ver -source $nuserver -noninteractive
-nuget add .\src\Punfai.Report\bin\Release\Punfai.Report.$ver.nupkg -source $nuserver
+dotnet pack .\src\Punfai.Report --configuration Release --output .\
 
 Write-Host -ForegroundColor Cyan "IronScript"
-dotnet pack .\src\Punfai.Report.IronScript --configuration Release
-nuget delete Punfai.Report.IronScript $ver -source $nuserver -noninteractive
-nuget add .\src\Punfai.Report.IronScript\bin\Release\Punfai.Report.IronScript.$ver.nupkg -source $nuserver
+dotnet pack .\src\Punfai.Report.IronScript --configuration Release --output .\
 
 Write-Host -ForegroundColor Cyan "OfficeOpenXml..."
-dotnet pack .\src\Punfai.Report.OfficeOpenXml --configuration Release
-nuget delete Punfai.Report.OfficeOpenXml $ver -source $nuserver -noninteractive
-nuget add .\src\Punfai.Report.OfficeOpenXml\bin\Release\Punfai.Report.OfficeOpenXml.$ver.nupkg -source $nuserver
+dotnet pack .\src\Punfai.Report.OfficeOpenXml --configuration Release --output .\
 
 Write-Host -ForegroundColor Cyan "Ibex..."
-nuget pack .\src\Punfai.Report.Ibex\Punfai.Report.Ibex.csproj -properties Configuration=Release
-nuget delete Punfai.Report.Ibex $ver -source $nuserver -noninteractive
-nuget add .\Punfai.Report.Ibex.$ver.nupkg -source $nuserver
-Remove-Item .\Punfai.Report.Ibex.$ver.nupkg
+nuget pack .\src\Punfai.Report.Ibex\Punfai.Report.Ibex.csproj -Build -properties Configuration=Release
 
 Write-Host -ForegroundColor Cyan "Wpf..."
-nuget pack .\src\Punfai.Report.Wpf\Punfai.Report.Wpf.csproj -properties Configuration=Release -symbols
-nuget delete Punfai.Report.Wpf $ver -source $nuserver -noninteractive
-nuget add .\Punfai.Report.Wpf.$ver.nupkg -source $nuserver
-Remove-Item .\Punfai.Report.Wpf.$ver.nupkg
-Remove-Item .\Punfai.Report.Wpf.$ver.symbols.nupkg
+nuget pack .\src\Punfai.Report.Wpf\Punfai.Report.Wpf.csproj -Build -properties Configuration=Release -symbols
